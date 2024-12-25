c8b53ad3c3a768e215c9ceba3ffb699c5d5b6c37
blazi <blazi@DESKTOP-9JBQAR7>
2024-12-24 18:50:28 -0800

    6:50 PM 12/24/2024 HAPPY NEW YEAR EVERYONE!!!!!!!!!!!!

--- src/_02_File_Encrypt_Decrypt/FileDecryptor.java
+++ src/_02_File_Encrypt_Decrypt/FileDecryptor.java
@@ -1,5 +1,12 @@
 package _02_File_Encrypt_Decrypt;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 public class FileDecryptor {
 	/*
 	 * Decryption is the process of taking encoded or encrypted text or other data
@@ -21,15 +28,67 @@ public class FileDecryptor {
 	 * Create a program that opens the file created by FileEncryptor and decrypts
 	 * the message, then display it to the user in a JOptionPane.
 	 */
-
+	
 	private String file;
 
-	public FileDecryptor(String file) {
-		this.file = file; 
-	}
-	//do encryptor FIRST
-	public String decrypt() {
-		return null;
+	public FileDecryptor() {
+		file = "src/_02_File_Encrypt_Decrypt/file.txt";
 	}
 	
+	public static void main(String[] args) {
+		FileDecryptor e = new FileDecryptor();
+		int userKey = Integer.parseInt(JOptionPane.showInputDialog("Shift value: "));
+		e.decrypt(userKey);
+	}
+
+	public void decrypt(int key) {
+		String input = fileReader(file);
+		key *= -1;
+		String output = "";
+
+		for (int i = 0; i < input.length(); i++) {
+			char c = input.charAt(i);
+			if (Character.isUpperCase(c)) {
+				c += key;
+				if (c > 'Z') {
+					c -= 26;
+				}
+				if (c < 'A') {
+					c += 26;
+				}
+			} else if (Character.isLowerCase(c)) {
+				c += key;
+				if (c > 'z') {
+					c -= 26;
+				}
+				if (c < 'a') {
+					c += 26;
+				}
+			}
+
+			output += c;
+		}
+
+		JOptionPane.showMessageDialog(null, "Decrypted message: " + output);
+	}
+
+	
+	public String fileReader(String file) {
+		String output = "";
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			
+			String line = br.readLine();
+			output += line;
+			br.close();
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return output;
+	}
 }
--- src/_02_File_Encrypt_Decrypt/FileEncryptor.java
+++ src/_02_File_Encrypt_Decrypt/FileEncryptor.java
@@ -1,13 +1,19 @@
 package _02_File_Encrypt_Decrypt;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+
+import javax.swing.JOptionPane;
+
 public class FileEncryptor {
 	/*
-	 * Encryption is the process of encoding a message or information
-	 * in such a way that only authorized parties can access it and
-	 * those who are not authorized cannot.
+	 * Encryption is the process of encoding a message or information in such a way
+	 * that only authorized parties can access it and those who are not authorized
+	 * cannot.
 	 *
-	 * A simple shift cipher works by shifting the letters of a message
-	 * down based on a key. If our key is 4 then:
+	 * A simple shift cipher works by shifting the letters of a message down based
+	 * on a key. If our key is 4 then:
 	 * 
 	 * a b c d e f g h i j k l m n o p q r s t u v w x y z
 	 * 
@@ -17,35 +23,63 @@ public class FileEncryptor {
 	 * 
 	 * "Hello World" changes to "Lipps Asvph"
 	 *
-	 * Create a program that takes a message and a key from the user.
-	 * Use the key to shift each letter in the users input and save the final result to a file.
+	 * Create a program that takes a message and a key from the user. Use the key to
+	 * shift each letter in the users input and save the final result to a file.
 	 */
-	
+
 	private String file;
-	private int key;
-	
-	public FileEncryptor(String file, int key) {
-		this.file = file;
-		this.key = key;
+
+	public static void main(String[] args) {
+		FileEncryptor e = new FileEncryptor();
+
+		String input = JOptionPane.showInputDialog("Message: ");
+		int userKey = Integer.parseInt(JOptionPane.showInputDialog("Shift value: "));
+		e.encryptToFile(input, userKey);
 	}
 
-	
-	
+	public FileEncryptor() {
+		file = "src/_02_File_Encrypt_Decrypt/file.txt";
+	}
+	//so it works like this ill type a message, set a shift value which will move letters up or down the alphabet
+	public void encryptToFile(String input, int key) {
+		try {
+			FileWriter w = new FileWriter(file);
+			w.write(encrypt(input, key));
+			w.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	public String encrypt(String input, int key) {
-		CharSequence cs = input;
-		return null;
+		String output = "";
+
+		for (int i = 0; i < input.length(); i++) {
+			char c = input.charAt(i);
+			if (Character.isUpperCase(c)) {
+				c += key;
+				if (c > 'Z') {
+					c -= 26;
+				}
+				if (c < 'A') {
+					c += 26;
+				}
+			} else if (Character.isLowerCase(c)) {
+				c += key;
+				if (c > 'z') {
+					c -= 26;
+				}
+				if (c < 'a') {
+					c += 26;
+				}
+			}
+
+			output += c;
+		}
+
+		System.out.println(output);
+
+		return output;
 	}
-	
-	
-	
-	
-	
-	
-	
-	//only use for debugging
-	public int getKey() {
-		return key;
-	}
-	
-	
+
 }
